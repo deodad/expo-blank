bd772bc31d8c172b04bafc8af94f889c1a5dc9a4
Tony D'Addeo <tony@merklemanufactory.com>
2024-08-29 15:16:35 -0700

    [expo-video] currentTime set in useVideoPlayer setup callback is ignored on Android

--- App.tsx
+++ App.tsx
@@ -1,20 +1,53 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { useVideoPlayer, VideoView } from 'expo-video';
+import { useRef, useState } from 'react';
+import { StyleSheet, View, Button } from 'react-native';
 
-export default function App() {
+const videoSource =
+  'https://commondatastorage.googleapis.com/gtv-videos-bucket/sample/BigBuckBunny.mp4';
+
+export default function VideoScreen() {
+  const ref = useRef(null);
+  const [isPlaying, setIsPlaying] = useState(true);
+  const player = useVideoPlayer(videoSource, player => {
+    player.loop = true;
+    player.currentTime = 10;
+    player.play();
+  });
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={styles.contentContainer}>
+      <VideoView
+        ref={ref}
+        style={styles.video}
+        player={player}
+        allowsFullscreen
+        allowsPictureInPicture
+      />
+      <View style={styles.controlsContainer}>
+        <Button
+          title={"Go to 10"}
+          onPress={() => {
+            player.currentTime = 10;
+          }}
+        />
+      </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  contentContainer: {
     flex: 1,
-    backgroundColor: '#fff',
+    padding: 10,
     alignItems: 'center',
     justifyContent: 'center',
+    paddingHorizontal: 50,
+  },
+  video: {
+    width: 350,
+    height: 275,
+  },
+  controlsContainer: {
+    padding: 10,
   },
 });
